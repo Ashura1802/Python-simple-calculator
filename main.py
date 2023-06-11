bdbf83c4735218e04375808a8c7d73624b2ead8a
Sahil S Trambadia <sahiltrambadia18@gmail.com>
2023-06-11 18:38:05 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+def main():
+    print("Simple Calculator")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    print("Select operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+
+    choice = input("Enter your choice (1-4): ")
+
+    if choice == '1':
+        result = add(num1, num2)
+        print("Result:", result)
+    elif choice == '2':
+        result = subtract(num1, num2)
+        print("Result:", result)
+    elif choice == '3':
+        result = multiply(num1, num2)
+        print("Result:", result)
+    elif choice == '4':
+        result = divide(num1, num2)
+        print("Result:", result)
+    else:
+        print("Invalid choice. Please enter 1 or 4.")
+
+
+if __name__ == '__main__':
+    main()
